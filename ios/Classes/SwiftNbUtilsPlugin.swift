e61b3e6230f2c5df5ffd0f9c00b22f3bef4c1e8d
skjun <skjun.java@gmail.com>
2023-02-27 21:58:50 +0800

    init 4.4.12

--- ios/Classes/SwiftNbUtilsPlugin.swift
+++ ios/Classes/SwiftNbUtilsPlugin.swift
@@ -2,8 +2,6 @@ import Flutter
 import UIKit
 
 public class SwiftNbUtilsPlugin: NSObject, FlutterPlugin {
-  var packInfo = [String: String]()
-
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "nb_utils", binaryMessenger: registrar.messenger())
     let instance = SwiftNbUtilsPlugin()
@@ -11,12 +9,6 @@ public class SwiftNbUtilsPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if(call.method == "packageInfo") {
-        self.packInfo = ["appName": Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as! String,"packageName": Bundle.main.bundleIdentifier!,"versionCode": Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String, "versionName": Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String];
-
-        result(self.packInfo);
-    } else {
-        result("")
-    }
+    result("iOS " + UIDevice.current.systemVersion)
   }
 }
